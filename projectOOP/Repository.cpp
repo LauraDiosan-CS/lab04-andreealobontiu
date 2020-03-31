60d2d731560c545753ce168bf922bb12cbbda26c
andreealobontiu <57100452+andreealobontiu@users.noreply.github.com>
2020-03-31 14:08:53 +0300

    Update Repository.cpp

--- projectOOP/Repository.cpp
+++ projectOOP/Repository.cpp
@@ -1,11 +1,29 @@
 #include "Repository.h"
 #include <string>
 
+//default constructor for Repository
+Repository::Repository()
+{
+	this->projects = new Project[100];
+	this->length = 0;
+}
+
+//destructor for Repository
+Repository::~Repository()
+{
+	if (this->projects!=NULL)
+	{
+		this->projects = NULL;
+		this->length = 0;
+	}
+}
+
 //function that adds an instance of Project to Repository
 //in: an instance of Project
 //out:- 
-int Repository::addProject(Project & p)
+int Repository::addProject(const char* g, int b, int c)
 {
+	Project p = Project(g, b, c);
 	int poz = verifDuplicate(p);
 	if (poz == -1)
 	{
@@ -19,28 +37,28 @@ int Repository::addProject(Project & p)
 //function that removes an instance of Project from Repository
 //in: an instance of Project
 //out:- 
-int Repository::delProject(Project & p)
+int Repository::delProject(Project p)
 {
 	int poz = verifDuplicate(p);
 	if (poz != -1)
 	{
-		for (int i = poz; i <= this->length - 1; i++)
+		for (int i = poz; i <= this->length - 2; i++)
 		{
 			this->projects[i] = this->projects[i + 1];
 		}
 		this->length = this->length - 1;
-		return 1;
 	}
-	else
-		return 0;
+	
+	return 1;
 }
 
 //function that checks if an instance of Project is duplicate
 //in: the instance of Project that will be checked
 //out: the position of the duplicate or -1 otherwise
-int Repository::verifDuplicate(Project & p)
+int Repository::verifDuplicate(Project p)
 {
-	for (int i = 0; i < this->length; i++)
+	int len = this->length;
+	for (int i = 0; i < len; i++)
 		if ((p.getNrBranches() == this->projects[i].getNrBranches()) && (strcmp(p.getPath(), this->projects[i].getPath()) == 0) && (p.getNrCommits() == projects[i].getNrCommits()))
 			return i;
 	return -1;
@@ -50,6 +68,17 @@ int Repository::verifDuplicate(Project & p)
 Project * Repository::getAll()
 {
 	return this->projects;
+}
+
+//setter for Repository
+void Repository::setAll(Project * p, int l)
+{
+	this->length = l;
+	for (int i = 0; i < this->length; i++) {
+
+		this->projects[i] = p[i];
+
+	}
 }
 
 //function that returns the length of Repository

68986207c507fbc379a731697c208e43c3e1fb6a
andreealobontiu <57100452+andreealobontiu@users.noreply.github.com>
2020-03-31 14:09:17 +0300

    Update Repository.h

--- projectOOP/Repository.h
+++ projectOOP/Repository.h
@@ -3,14 +3,16 @@
 class Repository
 {
 private:
-	int length = 0;
-	Project projects[100];
+	int length;
+	Project* projects;
 public:
-	Repository(){}
-	int addProject(Project& p);
-	int delProject(Project& p);
-	int verifDuplicate(Project& p);
+	Repository();
+	~Repository();
+	int addProject(const char* p, int b, int c);
+	int delProject(Project p);
+	int verifDuplicate(Project p);
 	Project* getAll();
+	void setAll(Project* p, int l);
 	int getLen();
 	int updateProject(int index, Project newP);
 };
